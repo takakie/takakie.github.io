69cf22aaf57b1b9f001f55b14f34a8805ad0af74
4060ti <jack964270261@gmail.com>
2023-12-11 02:39:52 +0800

    Site updated: 2023-12-11 02:39:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/10/SMMS自动图床/","2023/12/10/js-chrome-debug/","2023/12/10/fastjson-templatesimpl/","2023/12/10/picture/","2023/12/10/git-study/","2023/12/10/hello-world/","2023/12/10/springsecurity-login/"];function toRandomPost(){
+var posts=["2023/12/10/fastjson-templatesimpl/","2023/12/10/git-study/","2023/12/10/hello-world/","2023/12/10/js-chrome-debug/","2023/12/10/SMMS自动图床/","2023/12/10/springsecurity-login/","2023/12/10/picture/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
