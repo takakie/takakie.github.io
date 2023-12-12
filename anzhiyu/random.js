dacd30a825ee919726917753f213f84c61a9a786
4060ti <jack964270261@gmail.com>
2023-12-13 02:09:56 +0800

    Site updated: 2023-12-13 02:09:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/42355/","posts/12418/","posts/50056/","posts/16786/","posts/9111/","posts/38963/","posts/13908/","posts/64555/","posts/58107/"];function toRandomPost(){
+var posts=["posts/50056/","posts/42355/","posts/12418/","posts/9111/","posts/16786/","posts/13908/","posts/64555/","posts/38963/","posts/58107/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
