cb030878d8ac06c76032225c21522e21013fc983
gaomu <jack964270261@gmail.com>
2023-12-13 14:27:58 +0800

    Site updated: 2023-12-13 14:27:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/12418/","posts/59809/","posts/11729/","posts/16107/","posts/59148/","posts/6295/","posts/20272/"];function toRandomPost(){
+var posts=["posts/49984/","posts/42904/","posts/42355/","posts/50056/","posts/17531/","posts/12418/","posts/16786/","posts/47497/","posts/9111/","posts/54173/","posts/24779/","posts/38963/","posts/29310/","posts/13908/","posts/64555/","posts/58107/","posts/52505/","posts/5706/","posts/18208/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
