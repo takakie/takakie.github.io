458043dac9eda7576bed58038e93d084cdb78e89
4060ti <jack964270261@gmail.com>
2023-12-13 20:03:59 +0800

    Site updated: 2023-12-13 20:03:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/50056/","posts/42355/","posts/12418/","posts/9111/","posts/16786/","posts/13908/","posts/64555/","posts/38963/","posts/58107/"];function toRandomPost(){
+var posts=["posts/42355/","posts/50056/","posts/12418/","posts/17531/","posts/49984/","posts/9111/","posts/16786/","posts/54173/","posts/42904/","posts/47497/","posts/24779/","posts/38963/","posts/13908/","posts/64555/","posts/29310/","posts/58107/","posts/52505/","posts/5706/","posts/18208/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
