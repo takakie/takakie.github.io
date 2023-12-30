9c166d921cc90c60c0922d3c6eeb19ceaa420d59
4060ti <jack964270261@gmail.com>
2023-12-30 19:01:11 +0800

    Site updated: 2023-12-30 19:01:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/42904/","posts/45366/","posts/49984/","posts/37000/","posts/42355/","posts/50056/","posts/12418/","posts/28315/","posts/17531/","posts/47497/","posts/54173/","posts/16786/","posts/9111/","posts/24779/","posts/38963/","posts/13908/","posts/29310/","posts/64555/","posts/52505/","posts/58107/","posts/5706/","posts/18208/","posts/8029/","posts/29948/"];function toRandomPost(){
+var posts=["posts/49984/","posts/42904/","posts/45366/","posts/50056/","posts/37000/","posts/42355/","posts/28315/","posts/17531/","posts/16786/","posts/9111/","posts/47497/","posts/12418/","posts/54173/","posts/38963/","posts/29310/","posts/13908/","posts/64555/","posts/58107/","posts/24779/","posts/41661/","posts/52505/","posts/5706/","posts/49250/","posts/57370/","posts/3456/","posts/9622/","posts/4398/","posts/29948/","posts/50790/","posts/8029/","posts/18208/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
